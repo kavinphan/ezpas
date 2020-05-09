5de361adcd3cab60498174f0a41e09050ed724ca
Kavin Phan <phan.kavin@gmail.com>
2020-05-09 19:35:38 -0400

    Update puller pipe stats

--- src/main/java/com/kqp/ezpas/block/entity/pullerpipe/EnderPullerPipeBlockEntity.java
+++ src/main/java/com/kqp/ezpas/block/entity/pullerpipe/EnderPullerPipeBlockEntity.java
@@ -4,6 +4,6 @@ import com.kqp.ezpas.init.Ezpas;
 
 public class EnderPullerPipeBlockEntity extends PullerPipeBlockEntity {
     public EnderPullerPipeBlockEntity() {
-        super(Ezpas.ENDER_PP_BLOCK_ENTITY, 1, Integer.MAX_VALUE, 4);
+        super(Ezpas.ENDER_PP_BLOCK_ENTITY, 4, Integer.MAX_VALUE, 1);
     }
 }
