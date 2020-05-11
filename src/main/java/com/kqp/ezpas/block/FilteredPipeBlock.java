3357830899aa9b1f76fd2caf2a407673ca1c85e5
Kavin Phan <phan.kavin@gmail.com>
2020-05-10 23:56:14 -0400

    Cleanup some pipe code

--- src/main/java/com/kqp/ezpas/block/FilteredPipeBlock.java
+++ src/main/java/com/kqp/ezpas/block/FilteredPipeBlock.java
@@ -19,7 +19,6 @@ import net.minecraft.item.ItemPlacementContext;
 import net.minecraft.sound.BlockSoundGroup;
 import net.minecraft.state.StateManager;
 import net.minecraft.state.property.AbstractProperty;
-import net.minecraft.state.property.BooleanProperty;
 import net.minecraft.util.ActionResult;
 import net.minecraft.util.Hand;
 import net.minecraft.util.ItemScatterer;
@@ -37,42 +36,25 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.List;
-import java.util.Map;
 import java.util.Set;
 
 public class FilteredPipeBlock extends BlockWithEntity {
-    public static final BooleanProperty NORTH = BooleanProperty.of("north");
-    public static final BooleanProperty EAST = BooleanProperty.of("east");
-    public static final BooleanProperty SOUTH = BooleanProperty.of("south");
-    public static final BooleanProperty WEST = BooleanProperty.of("west");
-    public static final BooleanProperty UP = BooleanProperty.of("up");
-    public static final BooleanProperty DOWN = BooleanProperty.of("down");
-
-    public static final Map<Direction, BooleanProperty> PROP_MAP = Util.make(new HashMap(), map -> {
-        map.put(Direction.NORTH, NORTH);
-        map.put(Direction.EAST, EAST);
-        map.put(Direction.SOUTH, SOUTH);
-        map.put(Direction.WEST, WEST);
-        map.put(Direction.UP, UP);
-        map.put(Direction.DOWN, DOWN);
-    });
-
     private final ShapeUtil shapeUtil;
-    
+
     public final Type type;
 
     public FilteredPipeBlock(Type type) {
         super(FabricBlockSettings.of(Material.GLASS).strength(0.3F, 0.3F).nonOpaque().sounds(BlockSoundGroup.GLASS).nonOpaque().build());
 
         this.type = type;
-        
+
         this.setDefaultState(this.getStateManager().getDefaultState()
-                .with(NORTH, false)
-                .with(EAST, false)
-                .with(SOUTH, false)
-                .with(WEST, false)
-                .with(UP, false)
-                .with(DOWN, false)
+                .with(PipeBlock.NORTH, false)
+                .with(PipeBlock.EAST, false)
+                .with(PipeBlock.SOUTH, false)
+                .with(PipeBlock.WEST, false)
+                .with(PipeBlock.UP, false)
+                .with(PipeBlock.DOWN, false)
         );
 
         this.shapeUtil = new ShapeUtil(this);
@@ -141,7 +123,7 @@ public class FilteredPipeBlock extends BlockWithEntity {
     }
 
     public AbstractProperty<Boolean> getProperty(Direction facing) {
-        return PROP_MAP.get(facing);
+        return PipeBlock.PROP_MAP.get(facing);
     }
 
     private BlockState makeConnections(World world, BlockPos pos) {
@@ -152,8 +134,8 @@ public class FilteredPipeBlock extends BlockWithEntity {
         Boolean up = isConnectable(world, pos.up(), Direction.DOWN);
         Boolean down = isConnectable(world, pos.down(), Direction.UP);
 
-        return this.getDefaultState().with(NORTH, north).with(EAST, east)
-                .with(SOUTH, south).with(WEST, west).with(UP, up).with(DOWN, down);
+        return this.getDefaultState().with(PipeBlock.NORTH, north).with(PipeBlock.EAST, east)
+                .with(PipeBlock.SOUTH, south).with(PipeBlock.WEST, west).with(PipeBlock.UP, up).with(PipeBlock.DOWN, down);
     }
 
     @Override
@@ -166,7 +148,7 @@ public class FilteredPipeBlock extends BlockWithEntity {
 
     @Override
     protected void appendProperties(StateManager.Builder<Block, BlockState> builder) {
-        builder.add(NORTH, EAST, SOUTH, WEST, UP, DOWN);
+        builder.add(PipeBlock.NORTH, PipeBlock.EAST, PipeBlock.SOUTH, PipeBlock.WEST, PipeBlock.UP, PipeBlock.DOWN);
     }
 
     @Override
@@ -196,19 +178,19 @@ public class FilteredPipeBlock extends BlockWithEntity {
     /**
      * Thanks Tech Reborn :)
      * Slightly modified for my purposes, but most of it is thanks to the Tech Reborn peeps.
-     *
+     * <p>
      * Copyright (c) 2020 TechReborn
-     *
+     * <p>
      * Permission is hereby granted, free of charge, to any person obtaining a copy
      * of this software and associated documentation files (the "Software"), to deal
      * in the Software without restriction, including without limitation the rights
      * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
      * copies of the Software, and to permit persons to whom the Software is
      * furnished to do so, subject to the following conditions:
-     *
+     * <p>
      * The above copyright notice and this permission notice shall be included in all
      * copies or substantial portions of the Software.
-     *
+     * <p>
      * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
      * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
      * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
@@ -238,8 +220,8 @@ public class FilteredPipeBlock extends BlockWithEntity {
             final VoxelShape baseShape = Block.createCuboidShape(size, size, size, 16.0D - size, 16.0D - size, 16.0D - size);
 
             final List<VoxelShape> connections = new ArrayList<>();
-            for(Direction dir : Direction.values()){
-                if(state.get(PROP_MAP.get(dir))) {
+            for (Direction dir : Direction.values()) {
+                if (state.get(PipeBlock.PROP_MAP.get(dir))) {
                     double x = dir == Direction.WEST ? 0 : dir == Direction.EAST ? 16D : size;
                     double z = dir == Direction.NORTH ? 0 : dir == Direction.SOUTH ? 16D : size;
                     double y = dir == Direction.DOWN ? 0 : dir == Direction.UP ? 16D : size;
@@ -248,7 +230,7 @@ public class FilteredPipeBlock extends BlockWithEntity {
                     connections.add(shape);
                 }
             }
-            return VoxelShapes.union(baseShape, connections.toArray(new VoxelShape[]{}));
+            return VoxelShapes.union(baseShape, connections.toArray(new VoxelShape[] {}));
         }
 
         public VoxelShape getShape(BlockState state) {
--- src/main/java/com/kqp/ezpas/block/PipeBlock.java
+++ src/main/java/com/kqp/ezpas/block/PipeBlock.java
@@ -2,15 +2,11 @@ package com.kqp.ezpas.block;
 
 import com.kqp.ezpas.block.entity.pullerpipe.PullerPipeBlockEntity;
 import com.kqp.ezpas.block.pullerpipe.PullerPipeBlock;
-import com.kqp.ezpas.init.Ezpas;
-import net.fabricmc.api.EnvType;
-import net.fabricmc.api.Environment;
 import net.fabricmc.fabric.api.block.FabricBlockSettings;
 import net.minecraft.block.Block;
 import net.minecraft.block.BlockState;
 import net.minecraft.block.Material;
 import net.minecraft.entity.EntityContext;
-import net.minecraft.entity.EntityType;
 import net.minecraft.item.ItemPlacementContext;
 import net.minecraft.sound.BlockSoundGroup;
 import net.minecraft.state.StateManager;
@@ -121,19 +117,19 @@ public class PipeBlock extends Block {
     /**
      * Thanks Tech Reborn :)
      * Slightly modified for my purposes, but most of it is thanks to the Tech Reborn peeps.
-     *
+     * <p>
      * Copyright (c) 2020 TechReborn
-     *
+     * <p>
      * Permission is hereby granted, free of charge, to any person obtaining a copy
      * of this software and associated documentation files (the "Software"), to deal
      * in the Software without restriction, including without limitation the rights
      * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
      * copies of the Software, and to permit persons to whom the Software is
      * furnished to do so, subject to the following conditions:
-     *
+     * <p>
      * The above copyright notice and this permission notice shall be included in all
      * copies or substantial portions of the Software.
-     *
+     * <p>
      * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
      * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
      * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
@@ -163,8 +159,8 @@ public class PipeBlock extends Block {
             final VoxelShape baseShape = Block.createCuboidShape(size, size, size, 16.0D - size, 16.0D - size, 16.0D - size);
 
             final List<VoxelShape> connections = new ArrayList<>();
-            for(Direction dir : Direction.values()){
-                if(state.get(PROP_MAP.get(dir))) {
+            for (Direction dir : Direction.values()) {
+                if (state.get(PROP_MAP.get(dir))) {
                     double x = dir == Direction.WEST ? 0 : dir == Direction.EAST ? 16D : size;
                     double z = dir == Direction.NORTH ? 0 : dir == Direction.SOUTH ? 16D : size;
                     double y = dir == Direction.DOWN ? 0 : dir == Direction.UP ? 16D : size;
@@ -173,7 +169,7 @@ public class PipeBlock extends Block {
                     connections.add(shape);
                 }
             }
-            return VoxelShapes.union(baseShape, connections.toArray(new VoxelShape[]{}));
+            return VoxelShapes.union(baseShape, connections.toArray(new VoxelShape[] {}));
         }
 
         public VoxelShape getShape(BlockState state) {
